442a5fe013e167d87671ef0f2a60ffd28976785b
viox <m.d.mihailik@gmail.com>
2024-01-02 21:30:40 +0300

    feat: add nvim-cmp

---
+++ lua/viox/plugins/nvim-cmp.lua
@@ -0,0 +1,49 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path", -- for path autocompletion
+
+    "L3MON4D3/LuaSnip", -- snippet engine
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    
+    -- just a buch of usefull autocompletions for different languages
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    local luasnip = require("luasnip")
+
+    -- this was specified in friendly-snippets repository
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    cmp.setup({
+      completion = {
+        completeopt = 'menu,menuone,noinsert',
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+      },
+      sources = cmp.config.sources({
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+      })
+    })
+  end
+}
