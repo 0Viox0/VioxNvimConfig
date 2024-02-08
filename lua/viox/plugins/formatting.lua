46b6acf6644fa16dfac34d56f6dd193efec42655
viox <m.d.mihailik@gmail.com>
2024-02-08 23:54:48 +0300

    feat: linting

--- lua/viox/plugins/formatting.lua
+++ lua/viox/plugins/formatting.lua
@@ -1,35 +1,35 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				lua = { "stylua" },
-				cs = { "csharpier" },
-			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				async = false,
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                lua = { "stylua" },
+                cs = { "csharpier" },
+            },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                async = false,
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+        })
 
-		vim.keymap.set({ "n", "v" }, "<leader>f", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+        vim.keymap.set({ "n", "v" }, "<leader>f", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
