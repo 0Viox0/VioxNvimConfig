8f251d6a74e5dbceb8a52b07cfec661cd52a3486
max <m.d.mihailik@gmail.com>
2024-06-03 00:14:03 +0300

    feat: add alacritty configuration

--- lua/viox/core/options.lua
+++ lua/viox/core/options.lua
@@ -1,22 +1,22 @@
-vim.o.autoindent = true -- will use indent of the previous line when going to the new one
-vim.o.expandtab = true -- now we can configure the number of spaces used for <tab>
-vim.o.breakindent = true -- for indenting wrapped arguments in stuff
-vim.o.tabstop = 4 -- number of spaces used for <tab>
-vim.o.shiftwidth = 4 -- number of spaces used for autoindent
+vim.o.autoindent = true         -- will use indent of the previous line when going to the new one
+vim.o.expandtab = true          -- now we can configure the number of spaces used for <tab>
+vim.o.breakindent = true        -- for indenting wrapped arguments in stuff
+vim.o.tabstop = 4               -- number of spaces used for <tab>
+vim.o.shiftwidth = 4            -- number of spaces used for autoindent
 
-vim.o.hlsearch = false -- highlight on search
+vim.o.hlsearch = false          -- highlight on search
 
 vim.o.clipboard = "unnamedplus" -- sync clipboards between os and nvim
 
-vim.o.number = true -- for displaying the current line number instead of 0
-vim.o.relativenumber = true -- for relative numbers
+vim.o.number = true             -- for displaying the current line number instead of 0
+vim.o.relativenumber = true     -- for relative numbers
 
-vim.o.undofile = true -- saves undo file to undo when opening file again
+vim.o.undofile = true           -- saves undo file to undo when opening file again
 
-vim.o.ignorecase = true -- search with insensitive searching
-vim.o.smartcase = true -- unless \C or capital in search
+vim.o.ignorecase = true         -- search with insensitive searching
+vim.o.smartcase = true          -- unless \C or capital in search
 
-vim.wo.signcolumn = "yes" -- add sign collumn to the left by default
+vim.wo.signcolumn = "yes"       -- add sign collumn to the left by default
 -- for avoiding flickering and inconsistent pages
 
 vim.o.updatetime = 250 -- saving to swap file after not typing for 250ms in case of crashes
